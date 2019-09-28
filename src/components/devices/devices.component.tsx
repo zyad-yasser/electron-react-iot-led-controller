3fc81bdf451437548d071a877de8a4939f195935
Zyad Yasser <zyadyasser6@gmail.com>
2019-09-28 03:16:05 +0200

    Devices UI

--- src/components/devices/devices.component.tsx
+++ src/components/devices/devices.component.tsx
@@ -1,24 +1,30 @@
 import * as React from 'react';
-import "./title-bar.component.sass";
+import "./devices.component.sass";
+import { serialPortConfig } from 'src/constants';
 
 class DevicesComponent extends React.Component {
+  public state = {
+    online: false
+  }
   public render() {
     return (
-      <div className="titleBar">
-        <div className="overlay" />
-        <div className="logo">
-          <img src="images/iot-logo.png" height="16px" />
-          <div className="title">
-            RGB led strip controller
+      <div className="devices">
+        <div className="content">
+          <div className="left">
+            <img src="images/strip.png" height="40px" />
+            <div className="info">
+              Device: Arduino RGB led strip ({`${serialPortConfig.idVendor}/${serialPortConfig.idProduct}`})<br />
+              Status: <span className={this.state.online ? 'online': 'offline'}>{this.state.online ? 'Online': 'Offline'}</span>
+            </div>
           </div>
-        </div>
-        <div className="buttons">
-          <button className="btn btn-refresh">
-            <img height="12px" src="icons/refresh.png" />
-          </button>
-          <button className="btn btn-exit">
-            <img height="12px" src="icons/close.png" />
-          </button>
+          <div className="right">
+            {
+              // @ts-ignore
+              <button className="switch" disabled={this.state.online ? '' : 'disabled'}>
+                <img src="icons/switch.png" height="20px"/>
+              </button>
+            }
+          </div>
         </div>
       </div>
     );
--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,18 +1,23 @@
-import * as React from 'react';
-import TitlebarComponent from '../title-bar/title-bar.component';
+import * as React from "react";
+import TitlebarComponent from "../title-bar/title-bar.component";
+import DevicesComponent from "../devices/devices.component";
+import "./layout.component.sass";
 
 class LayoutComponent extends React.Component {
   public render() {
     return (
-      <>
-        <TitlebarComponent />
-        <div>
-          {this.props.children}
+      <div className="layout">
+        <div className="background">
+          <img width="100%" height="100%" src="images/bk.png" />
         </div>
-      </>
+        <div className="content">
+          <TitlebarComponent />
+          <DevicesComponent />
+          <div>{this.props.children}</div>
+        </div>
+      </div>
     );
   }
 }
 
 export default LayoutComponent;
-
